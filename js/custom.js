2044f8ca5832a9d732653bd4eb828946070626a7
Mohammad Masoudi <mohammad.masoudi59@gmail.com>
2021-04-17 15:23:52 +0430

    add modals

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,44 @@
 var vm = new Vue({
   el: "#app",
+  data: function(){
+    return {
+      modalTitle: '',
+      modalContentType: 0,
+      registerForm: {
+        fullName: '',
+        phone: '',
+        password: '',
+        passwrodConfirm: '',
+      },
+    }
+  },
+  methods: {
+    showLoginModal(){
+      this.modalTitle = 'ورود به حساب کاربری';
+      this.modalContentType = 1;
+      this.showModal();
+    },
+    closeModal(){
+      this.modalTitle = 0;
+      this.modalContentType = '';
+      
+        this.$refs.modal.style.opacity = 0;
+        this.$refs.modal.style.display = 'none';
+      
+      },
+    showRegisterModal(){
+      this.modalTitle = 'نام نویسی در سایت';
+      this.modalContentType = 2;
+      this.showModal();
+      
+    },
+    showModal(){
+      
+        this.$refs.modal.style.opacity = 1;
+        this.$refs.modal.style.display = 'block';
+      
+
+    }
+
+  },
 });
